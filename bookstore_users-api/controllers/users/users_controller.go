90aa9f389a66f75cc84bfd4d02b175c31a5c6968
Harry Wijaya <harry.wijaya@ovo.id>
2021-05-29 17:21:33 +0700

    fix post users

--- bookstore_users-api/controllers/users/users_controller.go
+++ bookstore_users-api/controllers/users/users_controller.go
@@ -2,6 +2,7 @@ package users
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/HarryChang30/bookstore/bookstore_users-api/domain/users"
 	"github.com/HarryChang30/bookstore/bookstore_users-api/services"
@@ -9,6 +10,15 @@ import (
 
 	"github.com/gin-gonic/gin"
 )
+
+func getUserId(userIdParam string) (int64, rest_errors.RestErr) {
+	userId, err := strconv.ParseInt(userIdParam, 10, 64)
+	if err != nil {
+		return 0, rest_errors.NewBadRequestError("user id should be a number")
+	}
+
+	return userId, nil
+}
 
 func CreateUser(c *gin.Context) {
 	var user users.User
@@ -24,9 +34,34 @@ func CreateUser(c *gin.Context) {
 		return
 	}
 
-	c.JSON(http.StatusCreated, result.Marshall(c.GetHeader("X-Public") == "true"))
+	c.JSON(http.StatusCreated, result.Marshall(true))
 }
 
 func GetUser(c *gin.Context) {}
 
-func SearchUser(c *gin.Context) {}
+func SearchUser(c *gin.Context) {
+	status := c.Query("status")
+
+	users, err := services.UsersService.SearchUser(status)
+	if err != nil {
+		c.JSON(err.Status(), err)
+		return
+	}
+
+	c.JSON(http.StatusOK, users.Marshall(true))
+}
+
+func DeleteUser(c *gin.Context) {
+	userId, err := getUserId(c.Param("user_id"))
+	if err != nil {
+		c.JSON(err.Status(), err)
+		return
+	}
+
+	if err := services.UsersService.DeleteUser(userId); err != nil {
+		c.JSON(err.Status(), err)
+		return
+	}
+
+	c.JSON(http.StatusOK, map[string]string{"status": "deleted"})
+}
